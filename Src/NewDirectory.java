19eb4b48222534594b8a0a7791b59235732ea930
Andres7540 <andres.mejia@qubitsenergy.com>
2025-12-02 22:46:04 -0500

    First commit

--- Src/NewDirectory.java
+++ Src/NewDirectory.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class NewDirectory{
     public static void main(String [] args){
-        System.out.println("Hola Mundo");
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.next();
+        String lastName = sc.next();
+        int age = sc.nextInt();
+
+        System.out.println("Bienvenid@ " + name + lastName + "/n Tienes " + " años");
     }
 }
